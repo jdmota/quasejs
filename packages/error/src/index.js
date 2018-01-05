6f11887c4f73f4e795d84f5164614a57a4a65e78
João Mota <jdmota@outlook.pt>
2018-01-05 22:38:55 +0000

    Error: update

--- packages/error/src/index.js
+++ packages/error/src/index.js
@@ -98,7 +98,7 @@ export function install( extractor ) {
       const Module = require( "module" );
       prevExtension = Module._extensions[ ".js" ];
       Module._extensions[ ".js" ] = function( module, filename ) {
-        const content = extractor.fs.getFileSync( filename );
+        const content = extractor._read( filename );
         module._compile( stripBOM( content ), filename );
       };
     }
