1dd7b5740239a4f7731b5469e2a01a0a02e99c5f
João Mota <jdmota@outlook.pt>
2018-01-24 17:07:05 +0000

    Builder: update cli

--- packages/builder/bin/index.js
+++ packages/builder/bin/index.js
@@ -1,25 +1,16 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
-
-const help = `
-Usage
-  $ quase-builder [options]
-
-Options
-  --watch, -w    Watch files for changes and re-build
-`;
 
 require( "@quase/cli" ).default( ( { options } ) => {
   require( "../dist" ).default( options );
 }, {
-  help,
+  usage: "$ quase-builder [options]",
   configFiles: "quase-builder-config.js",
   configKey: "quase-builder",
   schema: {
     watch: {
       type: "boolean",
       alias: "w",
-      default: false
+      description: "Watch files for changes and re-build"
     }
   }
 } );
