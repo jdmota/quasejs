b4e4398530598336c74b4e3d22f5808e0a4ade3b
João Mota <jdmota@outlook.pt>
2018-03-01 17:38:32 +0000

    Remove publish mode from the build script

--- packages/build.js
+++ packages/build.js
@@ -1,48 +1,22 @@
-const childProcess = require( "child_process" );
-const path = require( "path" );
 const fs = require( "fs-extra" );
+const path = require( "path" );
+const execa = require( "execa" );
 
-/* eslint-disable no-console, no-process-exit */
+/* eslint no-console: 0 */
+/* eslint no-process-exit: 0 */
 
-function run( command ) {
-  process.env.PATH = path.resolve( "./node_modules/.bin" ) + path.delimiter + process.env.PATH;
+const pkg = process.argv.slice( 2 )[ 0 ];
 
-  const commandArr = command.split( " " );
-  const child = childProcess.spawn( commandArr[ 0 ], commandArr.slice( 1 ), {
-    cwd: process.cwd(),
-    stdio: "inherit",
-    env: process.env,
-    shell: true
-  } );
-
-  child.on( "close", code => {
-    process.exitCode = code;
-  } );
-}
-
-const args = process.argv.slice( 2 );
-const publish = args[ 0 ] === "publish";
-const pkg = args[ 1 ];
-
-if ( typeof pkg !== "string" ) {
-  console.error( pkg + " is not a string." );
+if ( !pkg || typeof pkg !== "string" ) {
+  console.error( pkg + " is not valid" );
   process.exit( 1 );
 }
 
-const pkgFolder = path.join( "packages", pkg ).replace( /\\+/g, "/" );
 const src = path.join( "packages", pkg, "src" ).replace( /\\+/g, "/" );
 const dist = path.join( "packages", pkg, "dist" ).replace( /\\+/g, "/" );
 
-const babel = `babel ${src} --out-dir ${dist} --copy-files`;
+console.log( `Running build in ${pkg}...` );
 
-const canPublish = true;
-
-const publishCommand = canPublish ? "yarn publish --access public" : "echo Ignoring publishing!";
-
-const command = publish ?
-  `${babel} && cd ${pkgFolder} && yarn config set version-git-tag false && ${publishCommand} && echo Success!` :
-  `${babel} && echo Success!`;
-
-console.log( `Running '${command}' ...` );
-
-fs.emptyDir( dist ).then( () => run( command ) );
+fs.emptyDir( dist ).then( () => execa( "babel", [ src, "--out-dir", dist, "--copy-files" ], {
+  stdio: "inherit"
+} ) );
