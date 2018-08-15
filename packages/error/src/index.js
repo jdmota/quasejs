d08331fb5214f006073910742684bede2baddf74
João Mota <jdmota@outlook.pt>
2018-08-15 19:34:23 +0100

    Error: refactoring and tests

--- packages/error/src/index.js
+++ packages/error/src/index.js
@@ -19,9 +19,10 @@ export async function beautify( originalStack, extractor, options ) {
   const promises = frames.map( async( { fileName, functionName, args, lineNumber, columnNumber } ) => {
 
     const stackLine = {
-      textLine: `${functionName}${args ? `(${args.join( ", " )})` : ""}`,
+      textLine: "",
       file: fileName,
       code: null,
+      name: functionName,
       line: lineNumber,
       column: columnNumber
     };
@@ -33,6 +34,7 @@ export async function beautify( originalStack, extractor, options ) {
       } else {
         stackLine.file = pos.originalFile;
         stackLine.code = pos.originalCode;
+        stackLine.name = pos.name || stackLine.name;
         stackLine.line = pos.line;
         stackLine.column = pos.column;
       }
@@ -40,8 +42,8 @@ export async function beautify( originalStack, extractor, options ) {
       // Ignore
     }
 
+    stackLine.textLine = `${stackLine.name}${args ? `(${args.join( ", " )})` : ""}`;
     return stackLine;
-
   } );
 
   const cleaned = await Promise.all( promises );
@@ -57,6 +59,7 @@ export async function beautify( originalStack, extractor, options ) {
     stack,
     source: first && first.file ? first : null
   };
+  // source: ?{ file, code, name, line, column }
 }
 
 export function getStack( offset ) {
@@ -74,50 +77,6 @@ export function getStack( offset ) {
   }
 
   const arr = error.stack.split( "\n" );
-  arr.splice( 1, offset || 1 );
+  arr.splice( 1, offset > 1 ? offset : 1 );
   return arr.join( "\n" );
-}
-
-let installed = false;
-let prevPrepareStack;
-let prevExtension;
-
-function stripBOM( content ) {
-  if ( content.charCodeAt( 0 ) === 0xFEFF ) {
-    content = content.slice( 1 );
-  }
-  return content;
-}
-
-export function install( extractor ) {
-  if ( !installed ) {
-    installed = true;
-
-    prevPrepareStack = Error.prepareStackTrace;
-    Error.prepareStackTrace = function( error ) {
-      return beautify( error.stack, extractor );
-    };
-
-    if ( typeof require === "function" ) {
-      const Module = require( "module" );
-      prevExtension = Module._extensions[ ".js" ];
-      Module._extensions[ ".js" ] = function( module, filename ) {
-        const content = extractor._read( filename );
-        module._compile( stripBOM( content ), filename );
-      };
-    }
-  }
-}
-
-export function uninstall() {
-  if ( installed ) {
-    Error.prepareStackTrace = prevPrepareStack;
-    if ( prevExtension ) {
-      const Module = require( "module" );
-      Module._extensions[ ".js" ] = prevExtension;
-    }
-    prevPrepareStack = undefined;
-    prevExtension = undefined;
-    installed = false;
-  }
 }
---
+++ packages/error/test/index.js
@@ -0,0 +1,41 @@
+import { beautify, getStack } from "../src";
+
+const { SourceMapExtractor } = require( "@quase/source-map" );
+const fs = require( "fs-extra" );
+
+it( "getStack", () => {
+
+  const cwd = process.cwd();
+
+  function clean( string ) {
+    return string.split( cwd ).join( "" ).replace( /\\/g, "/" );
+  }
+
+  expect( clean( getStack() ) ).toMatchSnapshot( "undefined" );
+  expect( clean( getStack( -1 ) ) ).toMatchSnapshot( "-1" );
+  expect( clean( getStack( 0 ) ) ).toMatchSnapshot( "0" );
+  expect( clean( getStack( 1 ) ) ).toMatchSnapshot( "1" );
+  expect( clean( getStack( 2 ) ) ).toMatchSnapshot( "2" );
+  expect( clean( getStack( 3 ) ) ).toMatchSnapshot( "3" );
+
+} );
+
+it( "beautify", async() => {
+
+  const stack = getStack();
+  const extractor = new SourceMapExtractor( fs );
+
+  expect( ( await beautify( stack, extractor ) ).stack ).toMatchSnapshot( "dont ignore" );
+  expect( ( await beautify( stack, extractor, { ignore: /node_modules/ } ) ).stack ).toMatchSnapshot( "ignore node_modules" );
+
+} );
+
+it( "beautify with title", async() => {
+
+  const stack = new Error( "title" ).stack;
+  const extractor = new SourceMapExtractor( fs );
+
+  expect( ( await beautify( stack, extractor ) ).stack ).toMatchSnapshot( "dont ignore" );
+  expect( ( await beautify( stack, extractor, { ignore: /node_modules/ } ) ).stack ).toMatchSnapshot( "ignore node_modules" );
+
+} );
