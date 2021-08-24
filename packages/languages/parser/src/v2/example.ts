3f20c7384d8136b785a62aea2480d0e9f58d2200
jdmota <jdmota@outlook.pt>
2021-08-24 19:38:43 +0100

    Parser: wip

--- packages/languages/parser/src/v2/example.ts
+++ packages/languages/parser/src/v2/example.ts
@@ -96,13 +96,87 @@ const ruleD = rule(
   id("ret")
 );
 
+const ruleE = rule(
+  "E",
+  field("obj", object([["num", int(10)]])),
+  [],
+  {},
+  select(id("obj"), "num")
+);
+
 // const ruleB = seq(repeat(fieldMultiple("c", string("C"))), string("D"));
+
+// { x: T1, y: T2 } <: return
+// { x: T3, y: T4 } <: return
+// argument <: { x: T1 }
+// argument <: { y: T2 }
+// argument <: { x: T3 }
+// argument <: { y: T4 }
+
+// { x: T1, y: T2 } | { x: T3, y: T4 } <: return
+// argument <: { x: T1 & T3, y: T2 & T4 }
+
+// { x: unknown, y: bottom } <: return
+// { x: bottom, y: unknown } <: return
+// argument <: { x: unknown }
+// argument <: { y: bottom }
+// argument <: { x: bottom }
+// argument <: { y: unknown }
+
+// { x: unknown, y: bottom } | { x: bottom, y: unknown } <: return
+// argument <: { x: unknown & bottom, y: bottom & unknown }
+
+// TODO simplification:
+// 1. remove cycles
+// 2. remove the transitive edges
+// 3. if the super type is something like {x:T1}, we join it with another {x:?}
+
+// TODO idea:
+
+// { x: KeyOf<argument, x>, y: KeyOf<argument, y> } <: return
+// { x: KeyOf<argument, x>, y: KeyOf<argument, y> } <: return
+// argument <: { x: T1 }
+// argument <: { y: T2 }
+// argument <: { x: T3 }
+// argument <: { y: T4 }
+
+// TODO but we need to connect KeyOf<argument, x> with all the T1 and T3 in { x: T1 } and { x: T3 }
+// But maybe that means we don't need KeyOf<argument, x>, we can just do that directly!
+
+// TODO or not...
+
+//        A <: B
+// -----------------------
+// KeyOf<{x : A}, x> <: B
+
+//   O <: {x : A}     A <: B
+// ---------------------------
+//     KeyOf<O, x> <: B
+
+//   O <: {x : A}     B <: A
+// ---------------------------
+//     B <: KeyOf<O, x>
+
+// KeyOf<{x : A & B}, x> <: C
+// A & B <: C
+
+// ...
+
+// a <: b <: T2
+// a <: T1
+
+// How we connect T1 and T2?
+// What we can actually do is say that a & T1 <: T2
+// Because that is exactly what happens: for a member access, a <: {x:?}, but after that,
+// the type is refined so we intersect with {x:?}
+
+// If a <: T2 OR T1 <: T2 then a & T1 <: T2
 
 console.log("Starting...");
 
 const result = tool({
   name: "my_grammar",
-  ruleDecls: [ruleA, ruleB, ruleC, ruleD],
+  ruleDecls: [ruleA, ruleB, ruleC, ruleD, ruleE],
   tokenDecls: [],
 });
 
