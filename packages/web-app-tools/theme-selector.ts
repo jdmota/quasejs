f1a2dc8750eaebe47112e4d58d4b9d045e93c2eb
jdmota <jdmota@outlook.pt>
2025-07-28 17:00:39 +0100

    Web-app-tools: use storage event

--- packages/web-app-tools/theme-selector.ts
+++ packages/web-app-tools/theme-selector.ts
@@ -1,7 +1,6 @@
-import { localStorage } from "./support";
-import { AbstractDatabase } from "./database";
-import { Subscribable } from "./subscribable";
 import { noop } from "../util/miscellaneous";
+import { localStorage } from "./support";
+import { Subscribable } from "./subscribable";
 
 const DEFAULT_KEY = "theme-mode";
 
@@ -15,56 +14,11 @@ export type ThemeModeDetails = Readonly<{
   mode: ThemeMode;
 }>;
 
-type BroadcastMsg = Readonly<{
-  value: ThemeModeOrDefault;
-}>;
-
-function validate(value: string | null): ThemeModeOrDefault {
+function validate(value: string | null | undefined): ThemeModeOrDefault {
   return value === "light" || value === "dark" ? value : "default";
 }
 
-class ThemeModeSaved extends AbstractDatabase<BroadcastMsg> {
-  private key: string;
-  private value: ThemeModeOrDefault;
-
-  constructor(key: string) {
-    super(key);
-    this.key = key;
-    this.value = "default";
-  }
-
-  private getFromStorage() {
-    return validate(localStorage?.getItem(this.key) ?? this.value);
-  }
-
-  async reload() {
-    this.dataChange({ value: this.getFromStorage() }, false);
-  }
-
-  protected override dataChange(data: BroadcastMsg, broadcast: boolean): void {
-    this.value = data.value;
-    super.dataChange(data, broadcast);
-  }
-
-  getCached() {
-    return this.value;
-  }
-
-  get() {
-    this.value = this.getFromStorage();
-    return this.value;
-  }
-
-  set(value: ThemeModeOrDefault) {
-    localStorage?.setItem(this.key, value);
-    this.dataChange({ value }, true);
-  }
-}
-
-function convert(
-  theme: ThemeModeOrDefault | undefined,
-  prefersDark: boolean
-): ThemeMode {
+function convert(theme: ThemeModeOrDefault, prefersDark: boolean): ThemeMode {
   if (theme === "light" || theme === "dark") {
     return theme;
   }
@@ -72,37 +26,47 @@ function convert(
 }
 
 export class ThemeSelector extends Subscribable<ThemeModeDetails> {
-  private readonly db: ThemeModeSaved;
+  private readonly storageKey: string;
+  private savedValue: ThemeModeOrDefault;
   private prefersDark: boolean;
   private unsubPrefer: () => void;
-  private unsubDB: () => void;
+  private unsubStorage: () => void;
 
-  constructor(key: string = DEFAULT_KEY) {
+  constructor(storageKey: string = DEFAULT_KEY) {
     super();
-    this.db = new ThemeModeSaved(key);
+    this.storageKey = storageKey;
+    this.savedValue = "default";
     this.prefersDark = false;
     this.unsubPrefer = noop;
-    this.unsubDB = noop;
+    this.unsubStorage = noop;
   }
 
   get(): ThemeModeDetails {
-    const saved = this.db.getCached();
     return {
-      saved,
+      saved: this.savedValue,
       prefersDark: this.prefersDark,
-      mode: convert(saved, this.prefersDark),
+      mode: convert(this.savedValue, this.prefersDark),
     };
   }
 
   set(value: ThemeModeOrDefault) {
-    this.db.set(value);
+    this.internalSet(value);
+  }
+
+  private internalSet(value: string | null | undefined) {
+    const old = this.savedValue;
+    this.savedValue = validate(value);
+    if (this.savedValue !== old) {
+      localStorage?.setItem(this.storageKey, this.savedValue);
+      this.fire();
+    }
   }
 
   private fire() {
     this.emit(this.get());
   }
 
-  async init() {
+  init() {
     const prefersDarkQuery =
       typeof matchMedia === "function"
         ? matchMedia("(prefers-color-scheme: dark)")
@@ -121,15 +85,22 @@ export class ThemeSelector extends Subscribable<ThemeModeDetails> {
       };
     }
 
-    this.unsubDB = this.db.subscribe(event => this.fire());
+    const onStorageEvent = (evt: StorageEvent) => {
+      if (evt.key === this.storageKey && evt.storageArea === localStorage) {
+        this.internalSet(evt.newValue);
+      }
+    };
+    window.addEventListener("storage", onStorageEvent);
+    this.unsubStorage = () => {
+      window.removeEventListener("storage", onStorageEvent);
+    };
 
-    await this.db.reload();
+    this.internalSet(localStorage?.getItem(this.storageKey));
   }
 
   override close() {
     super.close();
-    this.db.close();
     this.unsubPrefer();
-    this.unsubDB();
+    this.unsubStorage();
   }
 }
