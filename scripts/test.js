319253f74cfabf361c1ee9dccf227f5c8d7e9ce9
jdmota <jdmota@outlook.pt>
2025-08-18 13:38:41 +0100

    Test script with debug

--- scripts/test.js
+++ scripts/test.js
@@ -9,12 +9,17 @@ if (args.length > 0 && !args[0].startsWith("-")) {
   args = args.slice(1);
 }
 
+const debug = args.includes("--debug");
+if (debug) args = args.filter(a => a !== "--debug");
+
 spawn(
   "node",
   [
     "--experimental-vm-modules",
     "--no-warnings=ExperimentalWarning",
+    debug ? "--inspect-brk" : "",
     "./node_modules/jest/bin/jest.js",
+    debug ? "--runInBand" : "",
     pkg ? `--roots=packages/${pkg}` : "",
     ...args,
   ].filter(Boolean),
